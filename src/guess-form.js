c94ee57b44af429a98d758b7114a7659f38d6ecd
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-27 15:57:28 -0400

    adds className to other components

--- src/guess-form.js
+++ src/guess-form.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function GuessForm(props) {
   return (
     <form className="guess-form">
-      <input type="number" />
-      <button>Guess</button>
+      <input type="number" className="guess-input" placeholder="enter your guess"/>
+      <button className="guess-button">Guess</button>
     </form>
   );
 }
