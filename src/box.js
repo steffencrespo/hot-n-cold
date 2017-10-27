fec2cc5b7ccc1b79607b299b239ab3fc4c333d6b
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-27 16:18:30 -0400

    modifies box component to make it a class component

--- src/box.js
+++ src/box.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './box.css';
 import GuessForm from './guess-form';
 import Counter from './counter';
 import GuessedNumber from './guessed-number';
 
-export default function Box(props) {
-  return (
-    <div className="box">
-      <header>
-        Status Component: Hot or Cold?
-      </header>
-      <div>
-        <GuessForm />
-        <Counter />
+export default class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      targetNumber: 0,
+      attemptedGuesses: 0
+    }
+  }
+
+  render() {
+    return (
+      <div className="box">
+        <header>
+          Status Component: Hot or Cold?
+        </header>
+        <div>
+          <GuessForm />
+          <Counter />
+        </div>
+        <div className="list">
+          <GuessedNumber number={1} /><GuessedNumber number={2} />
+        </div>
       </div>
-      <div className="list">
-        <GuessedNumber number={1} /><GuessedNumber number={2} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
