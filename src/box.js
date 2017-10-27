5d046bb57aa776d339b2b07a0294eabc43475816
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-27 15:54:46 -0400

    includes Counter component in the app

--- src/box.js
+++ src/box.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './box.css';
 import GuessForm from './guess-form';
+import Counter from './counter';
 
 export default function Box(props) {
   return (
@@ -10,7 +11,7 @@ export default function Box(props) {
       </header>
       <div>
         <GuessForm />
-        |counter|
+        <Counter />
       </div>
       <div>
         Counter Component
---
+++ src/counter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Counter(props) {
+  return(
+    <label>Guess #1</label>
+  );
+}
