bdb7b8fa81b341ebea8833bf2a55324eca9b3b74
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-30 15:07:47 -0400

    adding additional components to redistribute the elements more apropriately

---
+++ src/header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import TopNav from './top-nav';
+import InfoModal from './info-modal';
+
+import './header.css';
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfoModal: false
+    }
+  }
+
+  toggleInfoModal() {
+    this.setState({showInfoModal: !this.state.showInfoModal});
+  }
+
+  render() {
+    let infoModal;
+    if (this.state.showInfoModal) {
+      infoModal = <InfoModal onClose={() => this.toggleInfoModal()}/>;
+    }
+
+    return (
+      <header>
+        <TopNav onInfo={() => this.toggleInfoModal()}
+          onNewGame={() => this.props.onNewGame()} /> 
+        {infoModal}
+        <h1>HOT or COLD</h1>
+      </header>
+    );
+  }
+}
