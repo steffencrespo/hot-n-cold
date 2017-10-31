faf1a5bb8a18d6909bbcab898702d09e0798ffd8
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-31 15:34:37 -0400

    changes style to try to adjust the view

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import './reset.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './reset.css';
 import './index.css';
+
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
