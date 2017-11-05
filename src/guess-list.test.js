79d7c00a1fa05d62488f9e3e051495f5bc90d535
Leonardo Steffen <leosteffen84@gmail.com>
2017-11-05 10:52:24 -0500

    changes configs to deploy on CI

---
+++ src/guess-list.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import GuessList from './guess-list';
+
+describe('<GuessList />', () => {
+
+  it('Renders without crashing', () => {
+    const guessesList = [22, 23, 1];
+    shallow(<GuessList guesses={guessesList}/>);
+  });
+
+  it('Renders a list of guesses', () => {
+    const values = [10, 24, 52];
+    const wrapper = shallow(<GuessList guesses={values} />);
+    const items = wrapper.find('li');
+    expect(items.length).toEqual(values.length);
+    values.forEach((value, index) => {
+        expect(items.at(index).text()).toEqual(value.toString());
+    });
+  });
+  
+});
